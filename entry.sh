9daccd50917715fe8834527b62a4354786df3aed
Ve Ka <57703430+Ve-Ka@users.noreply.github.com>
2023-04-20 01:16:22 +0800

    Remove import DB
    It is not possible to import DB when the service is not active

--- entry.sh
+++ entry.sh
@@ -33,16 +33,6 @@ cat << EOF > $tfile
 EOF
 
 /usr/bin/mysqld --user=root --bootstrap --verbose=0 < $tfile
-rm -f $tfile && \
-fileCount=`ls -1 /home/*.sql 2>/dev/null | wc -l` && \
-if [ $fileCount != 0 ]; then 
-    /usr/bin/mysql -uroot -p"$MYSQL_ROOT_PASSWORD" anikatsu < /home/*.sql
-else
-    /usr/bin/mysql -uroot -p"$MYSQL_ROOT_PASSWORD" anikatsu < /var/www/localhost/htdocs/anikatsu.sql
-    echo `/usr/bin/mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e 'show databases'` >> /home/entry.log
-    echo 'sql command to add anikatsu.sql run' >> /home/entry.log
-fi 
+rm -f $tfile
 
 exec /usr/bin/mysqld --user=root --bind-address=127.0.0.1
-
-echo 'end of command' >> /home/entry.log
